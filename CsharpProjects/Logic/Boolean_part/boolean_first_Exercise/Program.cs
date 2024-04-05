0882917195856da946fa28d4ad9d5d86a81839e4
mugas <ricardo.d.moreira.rm@gmail.com>
2024-04-05 06:26:30 +0300

    logic exercises

---
+++ CsharpProjects/Logic/Boolean_part/boolean_first_Exercise/Program.cs
@@ -0,0 +1,34 @@
+﻿string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
+
+
+
+
+// string stage = permission.Contains("Admin") ? (level >= 55 ? "Welcome, Super Admin user." : "You do not have sufficient privileges.") : "You do not have sufficient privileges.";
---
+++ CsharpProjects/Logic/CodeLogic/training/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+bool flag = true;
+if (flag)
+{
+    int value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+here the variable doesnt exist
+Console.WriteLine($"Outside the code block: {value}");
+
+
+bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Outside the code block: {value}");
+
+// Code sample 1
+Here gives an erro because the compiler also analyzes the possibility of 
+flag=false and because the value was not assigned gives an error
+bool flag = true;
+int value;
+
+if (flag)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
+
+// Code sample 2
+
+//Here true is always true
+int value;
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
+*/
+
+
+bool flag = true;
+if (flag)
+{
+    Console.WriteLine(flag)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
